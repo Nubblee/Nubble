df915182735e685edc0c2574054c143fd7360bb2
Sonseongoh <nobleser@naver.com>
2024-11-10 15:18:34 +0900

    fix: 멤버일 경우에도 삭제 진행시 모달 띄우기 (#107)

--- src/components/comment/CommentList.tsx
+++ src/components/comment/CommentList.tsx
@@ -46,24 +46,56 @@ const CommentList = () => {
 	const handleDelete = async (commentId: number, type: 'MEMBER' | 'GUEST', password?: string) => {
 		try {
 			if (type === 'MEMBER') {
-				await axios.delete(
-					`http://nubble-backend-eb-1-env.eba-f5sb82hp.ap-northeast-2.elasticbeanstalk.com/comments/member/${commentId}`,
-					{
-						headers: { 'SESSION-ID': sessionId },
+				// 멤버는 default 타입 모달 띄우기
+				openModal({
+					type: 'default',
+					title: '댓글 삭제 확인',
+					onAction: async () => {
+						try {
+							// 실제 삭제 요청
+							await axios.delete(
+								`http://nubble-backend-eb-1-env.eba-f5sb82hp.ap-northeast-2.elasticbeanstalk.com/comments/member/${commentId}`,
+								{
+									headers: { 'SESSION-ID': sessionId },
+								},
+							)
+							// 삭제 후 상태 업데이트
+							setCommentsData((prevComments) =>
+								prevComments.filter((comment) => comment.commentId !== commentId),
+							)
+						} catch (error) {
+							console.error('댓글 삭제 중 에러 발생:', error)
+						}
 					},
-				)
+				})
 			} else {
-				// 비밀번호가 없는 경우 모달을 열어 비밀번호 요청
+				// 게스트는 password 타입 모달 띄우기
 				if (!password) {
 					openModal({
 						type: 'password',
 						title: '댓글 삭제 확인',
-						onAction: (inputPassword) => handleDelete(commentId, 'GUEST', inputPassword),
+						onAction: async (inputPassword) => {
+							try {
+								await axios.delete(
+									`http://nubble-backend-eb-1-env.eba-f5sb82hp.ap-northeast-2.elasticbeanstalk.com/comments/guest/${commentId}`,
+									{
+										headers: { 'Content-Type': 'application/json' },
+										data: { guestPassword: inputPassword },
+									},
+								)
+								// 삭제 후 상태 업데이트는 여기에서 실행
+								setCommentsData((prevComments) =>
+									prevComments.filter((comment) => comment.commentId !== commentId),
+								)
+							} catch (error) {
+								console.error('댓글 삭제 중 에러 발생:', error)
+							}
+						},
 					})
 					return
 				}
 
-				// 비밀번호가 있는 경우 삭제 요청 진행
+				// 비밀번호가 제공된 경우 삭제 진행
 				await axios.delete(
 					`http://nubble-backend-eb-1-env.eba-f5sb82hp.ap-northeast-2.elasticbeanstalk.com/comments/guest/${commentId}`,
 					{
@@ -71,17 +103,15 @@ const CommentList = () => {
 						data: { guestPassword: password },
 					},
 				)
+				// 삭제 후 상태 업데이트
+				setCommentsData((prevComments) =>
+					prevComments.filter((comment) => comment.commentId !== commentId),
+				)
 			}
-
-			// 댓글 삭제 후 상태 업데이트
-			setCommentsData((prevComments) =>
-				prevComments.filter((comment) => comment.commentId !== commentId),
-			)
 		} catch (error) {
 			console.error('댓글 삭제 중 에러 발생:', error)
 		}
 	}
-
 	if (error) return <p>{error}</p>
 	if (!commentsData.length) return <p>댓글이 없습니다.</p>
 
